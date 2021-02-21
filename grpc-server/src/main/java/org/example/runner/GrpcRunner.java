689ed3ca6a91b6225a431da65de16ed08042bd53
leehyounkyoo <leehyounkyoo@gmail.com>
2021-02-21 21:50:39 +0900

    .

--- grpc-server/src/main/java/org/example/runner/GrpcRunner.java
+++ grpc-server/src/main/java/org/example/runner/GrpcRunner.java
@@ -54,48 +54,33 @@ public class GrpcRunner implements ApplicationRunner
 //            
 //            log.info("GrpcRunner#stop - gRPC server was stopped");
 //        }));
+//        
+//        SERVER.awaitTermination();
 //    }
     
-//    private Server server;
-//    
-//    private void start() throws IOException {
-//    	 /* The port on which the server should run */
-//    	 int port = 3030;
-//    	 server = ServerBuilder.forPort(port).addService(new SampleServiceImpl()).build().start();
-//    	 
-//    	 log.info("Server started, listening on " + port);
-//    	 
-//    	 Runtime.getRuntime()
-//    	   .addShutdownHook(
-//    	     new Thread(
-//    	       () -> {
-//    	        // Use stderr here since the logger may have been reset by its JVM shutdown hook.
-//    	    	   log.info("*** shutting down gRPC server since JVM is shutting down");
-//    	        
-//    	        GrpcRunner.this.stop();
-//    	        
-//    	        log.info("*** server shut down");
-//    	       }));
-//    	}
-//    
-//    private void stop() {
-//        if (server != null) {
-//          server.shutdown();
-//        }
-//      }
-    
-    public void start() {
+    public void start() 
+    {
     	 Runtime.getRuntime().addShutdownHook(new Thread(SERVER::shutdown));
-    	 try {
+    	 
+    	 try 
+    	 {
     		 SERVER.start();
+    		 
+    		 log.info("GrpcRunner#start - listen port = {}", PORT);
+    		 
     		 SERVER.awaitTermination();
-    	 } catch (IOException e) {
-    	  throw new IllegalStateException("Unable to start grpc server.", e);
-    	 } catch (InterruptedException e) {
-    		 log.error("grpc server was interrupted.", e);
-    	  Thread.currentThread().interrupt();
+    	 } 
+    	 catch (IOException e) 
+    	 {
+    		 throw new IllegalStateException("Unable to start grpc server.", e);
+    	 } 
+    	 catch (InterruptedException e) 
+    	 {
+    		 log.info("grpc server was interrupted.", e);
+    		 
+    		 Thread.currentThread().interrupt();
     	 }
-    	}
+    }
 }
 
 
