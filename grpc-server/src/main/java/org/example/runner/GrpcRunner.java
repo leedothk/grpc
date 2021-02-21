efc763b45eef5fc459ca5b2fa8ccf7930a75c642
leehyounkyoo <leehyounkyoo@gmail.com>
2021-02-21 21:32:00 +0900

    .

--- grpc-server/src/main/java/org/example/runner/GrpcRunner.java
+++ grpc-server/src/main/java/org/example/runner/GrpcRunner.java
@@ -30,29 +30,72 @@ public class GrpcRunner implements ApplicationRunner
     	this.start();
     }
    
-    private void start() throws IOException, InterruptedException 
-    {
-        SERVER.start();
-
-        log.info("GrpcRunner#start - listen port = {}", PORT);
-
-        SERVER.awaitTermination();
-        
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> 
-        {
-        	log.info("GrpcRunner#stop - Stopping gRPC server");
-            
-        	try 
-        	{
-                SERVER.shutdown().awaitTermination(5, TimeUnit.SECONDS);
-            } 
-        	catch (InterruptedException e) 
-        	{
-                e.printStackTrace();
-            }
-            
-            log.info("GrpcRunner#stop - gRPC server was stopped");
-        }));
-    }
+//    private void start() throws IOException, InterruptedException 
+//    {
+//        SERVER.start();
+//
+//        log.info("GrpcRunner#start - listen port = {}", PORT);
+//
+//        //SERVER.awaitTermination();
+//        
+//        Runtime.getRuntime().addShutdownHook(new Thread(() -> 
+//        {
+//        	log.info("GrpcRunner#addShutdownHook()");
+//            
+//        	try 
+//        	{
+//                //SERVER.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+//        		SERVER.awaitTermination();
+//            } 
+//        	catch (InterruptedException e) 
+//        	{
+//                e.printStackTrace();
+//            }
+//            
+//            log.info("GrpcRunner#stop - gRPC server was stopped");
+//        }));
+//    }
+    
+//    private Server server;
+//    
+//    private void start() throws IOException {
+//    	 /* The port on which the server should run */
+//    	 int port = 3030;
+//    	 server = ServerBuilder.forPort(port).addService(new SampleServiceImpl()).build().start();
+//    	 
+//    	 log.info("Server started, listening on " + port);
+//    	 
+//    	 Runtime.getRuntime()
+//    	   .addShutdownHook(
+//    	     new Thread(
+//    	       () -> {
+//    	        // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+//    	    	   log.info("*** shutting down gRPC server since JVM is shutting down");
+//    	        
+//    	        GrpcRunner.this.stop();
+//    	        
+//    	        log.info("*** server shut down");
+//    	       }));
+//    	}
+//    
+//    private void stop() {
+//        if (server != null) {
+//          server.shutdown();
+//        }
+//      }
+    
+    public void start() {
+    	 Runtime.getRuntime().addShutdownHook(new Thread(SERVER::shutdown));
+    	 try {
+    		 SERVER.start();
+    		 SERVER.awaitTermination();
+    	 } catch (IOException e) {
+    	  throw new IllegalStateException("Unable to start grpc server.", e);
+    	 } catch (InterruptedException e) {
+    		 log.error("grpc server was interrupted.", e);
+    	  Thread.currentThread().interrupt();
+    	 }
+    	}
 }
+
 
